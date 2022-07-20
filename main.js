1d01158beaa013ed5875482ea1f13ba68384d0c6
joksa19 <nikolajoksimovic95@gmail.com>
2022-07-20 13:13:27 +0200

    Parallax background and movement completed.

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+import Layer from './layer.js';
+
 window.addEventListener('load', function(){
     const canvasEl = this.document.getElementById('canvas1');
     const ctx = canvasEl.getContext('2d');
-    canvasEl.width = 600;
-    canvasEl.height = 600;
+    canvasEl.width = 1024;
+    canvasEl.height = 800;
 
 
+    // PLAYER ANIMATIONS
     const playerImageEl = this.document.getElementById("player-img");
     const spriteWidth = 250;
     const spriteHeight = 200;
     
     const animationSelectEl = this.document.getElementById("animations-select");
-    let playerState = 'attack';
+    let playerState = 'run';
     animationSelectEl.addEventListener("change", (e)=>{
         playerState = e.target.value;
     });
@@ -75,20 +78,61 @@ window.addEventListener('load', function(){
         spriteAnimations[state.name] = frames;
     });
 
-    // console.log(spriteAnimations);
+    
+    // Parallax BACKGROUND
+
+    const gameSpeedSliderrEl = this.document.querySelector(".slider");
+    const sliderValueTextEl = this.document.getElementById("sliderValueText");
+    sliderValueTextEl.innerHTML = `Game speed: ${1}`;
+    gameSpeedSliderrEl.addEventListener("change", function(e){
+        sliderValueTextEl.innerHTML = `Game speed: ${e.target.value}`;
+    });
+
+    const backgorundLayer1 = new Image();
+    backgorundLayer1.src = "./assets/parallaxBackground/layer-1.png";
+    const backgorundLayer2 = new Image();
+    backgorundLayer2.src = "./assets/parallaxBackground/layer-2.png";
+    const backgorundLayer3 = new Image();
+    backgorundLayer3.src = "./assets/parallaxBackground/layer-3.png";
+    const backgorundLayer4 = new Image();
+    backgorundLayer4.src = "./assets/parallaxBackground/layer-4.png";
+    const backgorundLayer5 = new Image();
+    backgorundLayer5.src = "./assets/parallaxBackground/layer-5.png";
+    
+    let gameSpeed = 5;
+    gameSpeedSliderrEl.addEventListener("change", function(e){
+        gameSpeed = e.target.value;
+        sliderValueTextEl.innerHTML = `Game speed: ${gameSpeed}`;
+    });
+
+    const layer1 = new Layer(backgorundLayer1, 1, gameSpeed, ctx);
+    const layer2 = new Layer(backgorundLayer2, 0.1, gameSpeed, ctx);
+    const layer3 = new Layer(backgorundLayer3, 0.2, gameSpeed, ctx);
+    const layer4 = new Layer(backgorundLayer4, 0.7, gameSpeed, ctx);
+    const layer5 = new Layer(backgorundLayer5, 1, gameSpeed, ctx);
+
+    let layers = [layer1, layer2, layer3, layer4, layer5
+    ];
 
     
+    // "RENDERING"
     let gameFrame = 0;
     const staggerFrames = 5;
 
     function animate(){
-        ctx.clearRect(0,0,canvasEl.width, canvasEl.height);
+        ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
 
         let position = Math.floor(gameFrame/staggerFrames)%spriteAnimations[playerState].loc.length;
-        let frameX = spriteWidth*position;//position je vec od 0-6 pa je isto da li samo mnozimo sa position ili trazimo loc[postition].x..
+        let frameX = spriteWidth*position;
         let frameY = spriteAnimations[playerState].loc[position].y;
 
-        ctx.drawImage(playerImageEl, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+        //background
+        layers.forEach((layer)=>{
+            layer.update(gameSpeed,-gameFrame);
+            layer.draw();
+        });
+        //player
+        ctx.drawImage(playerImageEl, frameX, frameY, spriteWidth, spriteHeight, 0, 450, spriteWidth, spriteHeight);
         
         gameFrame++;
         requestAnimationFrame(animate);
@@ -96,3 +140,4 @@ window.addEventListener('load', function(){
     
     animate();
 });
+
