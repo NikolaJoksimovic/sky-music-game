56dfb2a398e2755e740ff768850403b8d2ca1e64
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-11 23:45:17 +0200

    fix the volume of letibudaleti intro

--- scrpit.js
+++ scrpit.js
@@ -74,6 +74,7 @@ window.addEventListener("load", function () {
     this,
     "./assets/audio/letibudaleti_intro.wav"
   );
+  letibudaletiIntro.audio.volume = 0.35;
   letibudaletiBtnEl.addEventListener("click", (e) => {
     letibudaletiBtnEl.style.display = "none";
     preloader.classList.add("preloader-show");
